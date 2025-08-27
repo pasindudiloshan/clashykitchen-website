ee74b4d654f59ba5bf8152cfc9e84cc8dd491cb0
PasinduDiloshan27 <pasindudiloshan@gmail.com>
2025-08-27 09:48:17 +0530

    add admin dashboard UI and navigation

---
+++ app/aboutus.php
@@ -0,0 +1,238 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   
+    
+    <title>About Us</title>
+    <link rel="stylesheet" href="css/aboutus.css">
+    
+    
+    
+    <link rel="stylesheet" href="fontsicon/css/all.css.css">
+
+    <link rel="shortcut icon" type="image" href="./image/logo.jpg">
+
+</head>
+
+<body>
+
+    <?php
+    include 'navbar.php';
+    ?>
+
+    
+    
+    <div class="image-container">
+        <img src="image/tabelbooking-banner.jpg">
+        <div class="overlay">
+            <p class="image-text">About Us</p>
+        </div>
+        </div>
+
+
+        <section id="aboutus">
+            <div class="about-container">
+                <section class="about">
+                    <div class="about-image">
+                        <img src="image/aboutus_banner.jpg">
+                          </div>
+                           <div class="about-content">
+                           <h2>Clashy Kitchen <span>Restaurant</span></h2>
+                           <p>Nestled amidst the lush landscapes and vibrant culture of Ja-Ela, Claashy Kitchen emerged as a beacon of culinary
+                             delight, rooted in the rich heritage of Sri Lankan flavors. Our journey began in the bustling streets of Ja-Ela,
+                             where the aroma of spices danced in the air and the spirit of hospitality thrived.
+                            Founded by a group of passionate food enthusiasts, Claashy Kitchen was born from a shared love for the diverse
+                             and flavorful cuisine of Sri Lanka. Drawing inspiration from the bustling markets and traditional recipes passed 
+                             down through generations, our founders set out to create a dining experience that honored the authenticity of Sri Lankan 
+                             cuisine while embracing innovation and creativity.</p>
+
+                           <p> With humble beginnings in a quaint corner of Ja-Ela, Claashy Kitchen quickly gained recognition for its commitment 
+                            to quality, flavor, and hospitality. From the fragrant curries of the north to the fiery delights of the south, every
+                             dish served at Claashy Kitchen tells a story of tradition, passion, and the vibrant tapestry of Sri Lankan culture.
+                            As we continue to evolve and grow, our dedication to serving authentic Sri Lankan cuisine remains unwavering.
+                             Claashy Kitchen is not just a restaurant; it's a celebration of the culinary heritage of Ja-Ela and a testament to the flavors that unite us all.
+                            Join us on a journey through the tantalizing tastes of Sri Lanka, right here at Claashy Kitchen.</p> 
+                  </div>
+                </section>
+            </div>
+        </section>
+
+        <div class="section-one">
+            <div class="row-right">
+                <div class="content-wrapper">
+                    <div class="content">
+                        <h2>Our <span>Manager</span></h2>
+                        <p>I'm Pasindu Diloshan, As the manager and sole proprietor of our esteemed establishment, I've poured seven years of dedication, passion, and perseverance into shaping our restaurant into the gem it is today. Through every challenge and triumph, I've remained steadfast in my commitment to excellence, and it's with immense pride that I stand at the helm of this culinary haven.
+
+                            Our journey has been one of growth, learning, and evolution, and I am deeply grateful for the unwavering support of our loyal patrons who have stood by us through thick and thin. Their satisfaction has always been my driving force, spurring me to continuously elevate our standards and offerings.
+                        </p>
+        
+                        <p>Today, I am thrilled to share our story with you through our website, a testament to our unwavering dedication to providing unparalleled dining experiences. With every dish meticulously curated to tantalize your taste buds and every detail crafted to ensure your utmost satisfaction, we invite you to indulge in the flavors of our culinary passion.
+
+                            Join us on a journey of gastronomic delight, where each visit promises to be a memorable experience filled with warmth, flavor, and the unmistakable essence of hospitality that defines us. Welcome to our world, where every meal is not just a feast for the senses but a celebration of the journey that brought us here.
+                        </p>
+                    </div>
+                </div>
+                <div class="image-wrapper">
+                    <img src="image/manager.png" alt="">
+                </div>
+            </div>
+        </div>
+
+
+
+
+        <div class="wrapper">
+              <div class="title">
+                    <h2>Our Team <span>Section</span></h2>
+                </div>
+
+                <div class="card_Container">
+
+                    <div class="card">
+
+                        <div class="imbBx">
+                            <img src="image/head chef.png" alt="">
+                        </div>
+
+                        <div class="content">
+                            <div class="contentBx">
+                                <h3>Kamal Rupasinghe<br><span>Head Chef</span></h3>
+                            </div>
+                            <ul class="sci">
+                            <li style="--i: 1">
+                                    <a href="#"><i class="fa-brands fa-facebook"></i></a>
+                                </li>
+                                <li style="--i: 2">
+                                    <a href="#"><i class="fa-brands fa-instagram"></i></a>
+                                </li>
+                                <li style="--i: 3">
+                                    <a href="#"><i class="fa-brands fa-linkedin-in"></i></a>
+                                </li>
+                            </ul>
+                        </div>
+
+                    </div>
+
+                    <div class="card">
+
+                        <div class="imbBx">
+                            <img src="image/Sous Chef.png" alt="">
+                        </div>
+
+                        <div class="content">
+                            <div class="contentBx">
+                                <h3>Kasun Perera<br><span>Sous Chef</span></h3>
+                            </div>
+                            <ul class="sci">
+                            <li style="--i: 1">
+                                    <a href="#"><i class="fa-brands fa-facebook"></i></a>
+                                </li>
+                                <li style="--i: 2">
+                                    <a href="#"><i class="fa-brands fa-instagram"></i></a>
+                                </li>
+                                <li style="--i: 3">
+                                    <a href="#"><i class="fa-brands fa-linkedin-in"></i></a>
+                                </li>
+                            </ul>
+                        </div>
+
+                    </div>
+
+                    <div class="card">
+
+                        <div class="imbBx">
+                            <img src="image/House Manager.png" alt="">
+                        </div>
+
+                        <div class="content">
+                            <div class="contentBx">
+                                <h3>Shyamalee Lowe<br><span>House Manager</span></h3>
+                            </div>
+                            <ul class="sci">
+                            <li style="--i: 1">
+                                    <a href="#"><i class="fa-brands fa-facebook"></i></a>
+                                </li>
+                                <li style="--i: 2">
+                                    <a href="#"><i class="fa-brands fa-instagram"></i></a>
+                                </li>
+                                <li style="--i: 3">
+                                    <a href="#"><i class="fa-brands fa-linkedin-in"></i></a>
+                                </li>
+                            </ul>
+                        </div>
+
+                    </div>
+
+                    <div class="card">
+
+                        <div class="imbBx">
+                            <img src="image/Accountant.png" alt="">
+                        </div>
+
+                        <div class="content">
+                            <div class="contentBx">
+                                <h3>Aruna Priyanjith<br><span>Accountant</span></h3>
+                            </div>
+                            <ul class="sci">
+                                <li style="--i: 1">
+                                    <a href="#"><i class="fa-brands fa-facebook"></i></a>
+                                </li>
+                                <li style="--i: 2">
+                                    <a href="#"><i class="fa-brands fa-instagram"></i></a>
+                                </li>
+                                <li style="--i: 3">
+                                    <a href="#"><i class="fa-brands fa-linkedin-in"></i></a>
+                                </li>
+                            </ul>
+                        </div>
+
+                    </div>
+                </div>
+             </div>
+                   
+             <div class="section-one">
+            <div class="row-right">
+                <div class="content-wrapper">
+                    <div class="content">
+                        <h2>Our <span>Location</span></h2>
+                        <p>Nestled in the heart of the Western Province, within the vibrant city of Ja-Ela, Clashy Kitchen stands as a culinary
+                            beacon in the bustling streets of Gampaha District. Situated as the main restaurant in Ja-Ela, our establishment 
+                            proudly serves as a cornerstone of the local dining scene. With a prime location that captures the essence of the 
+                            city's energy and charm, Clashy Kitchen invites patrons to embark on a gastronomic journey like no other. From our
+                            welcoming ambiance to our diverse menu offerings, every visit promises an unforgettable experience that celebrates 
+                            the rich tapestry of Sri Lankan cuisine. Join us at Clashy Kitchen and discover why we're not just a restaurant but
+                            a beloved destination in Ja-Ela.</p>
+                    </div>
+                </div>
+                <div class="image-wrapper">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3959.3733125118324!2d79.90051594325155!3d7.082648119668579!2m3!
+                    1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ae2f9e3eab1a4c9%3A0x87af2e6c94872d3a!2sThe%20Pearl%20Villa!5e0!3m2!1sen!2slk!4v1715797674633!5m2!1sen!2slk" 
+                    width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                </div>
+            </div>
+        </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            <?php
+            include 'footer.php';
+            ?>
+
+
+</body>
+</html>
